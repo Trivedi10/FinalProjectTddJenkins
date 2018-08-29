eae84a1c2f1e16e1b9c37c524b0907c7a78b6f99
Trivedi10 <41231531+Trivedi10@users.noreply.github.com>
2018-08-29 22:22:16 +0530

    Update NewTest.java

--- pipeline_test/src/test/java/test_pipeline/NewTest.java
+++ pipeline_test/src/test/java/test_pipeline/NewTest.java
@@ -5,6 +5,8 @@ import org.testng.annotations.BeforeTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 
 public class NewTest {
