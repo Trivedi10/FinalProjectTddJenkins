6c59ef3c199df1e768daade2ab59040b1d95f6b8
kapil <kapilrajtrivedi.nmims@gmail.com>
2018-08-29 18:18:03 +0530

    Commit 1

---
+++ pipeline_test/src/test/java/test_pipeline/NewTest.java
@@ -0,0 +1,38 @@
+package test_pipeline;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+	WebDriver x;
+	String title;
+	 @BeforeTest
+	  public void beforeTest() {
+		  System.setProperty("webdriver.gecko.driver", "E:\\geckodriver\\geckodriver.exe");
+		  x= new FirefoxDriver();
+		  x.get("http://18.222.29.115:8090/BloggingApplication-0.0.1-SNAPSHOT/");
+		  
+	  }
+	@Test
+  public void f() {
+	  //System.out.println("came");
+	  x.findElement(By.xpath("//*[ @name='title'] ")).sendKeys("narri");
+	  x.findElement(By.xpath("//*[ @value='send_title'] ")).click();
+	  
+	 // x.findElement(By.xpath("//*[ @name='category'] ")).sendKeys("narri");
+	  //x.findElement(By.xpath("//*[ @name='author'] ")).sendKeys("narri");
+	  
+	  title = x.getTitle();
+	  
+	  System.out.println("Title = "+title);
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
